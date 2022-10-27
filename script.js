0710df1275d9fdbe248087cde5d7be6cf2feb9cd
Edgar Veress <veress.edgar@yahoo.com>
2022-10-27 19:49:43 +0100

    css pt6 + functionality to add color depending on priority

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 const database = [];
 
 // Factory function for creating entries and their properties
-const CreateEntry = (title, description, dueDate) => {
-    return { title, description, dueDate };
+const CreateEntry = (title, description, dueDate, priority, profile) => {
+    return { title, description, dueDate, priority, profile };
 };
 
 // Query selectors
@@ -20,27 +20,44 @@ addNewBtn.addEventListener('click', () => {
     createEntryDiv.classList.add('entry');
     createEntryDiv.setAttribute('id', 'entry');
     createEntryDiv.innerHTML = `
-    <div class="title-form"><h5 id="model-Title">New Entry</h5></div>
-    <div class="left-side-form">
-        <textarea class="title-div" id="title" name="title" placeholder="Title:"></textarea>
-        <textarea class="desc-div" id="desc" name="desc" placeholder="Description:"></textarea>
+    <div class="modal-header">
+        <h3 class="modal-title">New Entry</h3>
+        <button class="modal-close">X</button>
     </div>
-    <div class="right-side-form">
-        <div class="date-div">Date: <input type="text" id="dueDate" name="dueDate"></div>
-        <div class="priority-div">
-            <label for="taskPriority">Priority: </label>
-            <select class="custom-select" id="taskPriority" required="">
-                <option value="low" selected="">Low</option>
-                <option value="medium">Medium</option>
-                <option value="high">High</option>
-            </select>
+    <div class="modal-body">
+        <div class="left-side">
+            <div class="form-textarea">
+                <label for="taskTitle">Title: </label>
+                <textarea class="taskTitle" id="title" name="taskTitle"></textarea>
+            </div>
+            <div class="form-textarea">
+                <label for="taskDesc">Description: </label>
+                <textarea class="taskDesc" id="desc" name="taskDesc"></textarea>
+            </div>
         </div>
-        <div class="project-div">
-            <label for="taskProject">Profile: </label>
-            <select class="custom-select" id="taskProject" required=""><option data-newtasktargetproject="_elx98r9i0">Inbox</option></select>
+        <div class="right-side">
+            <div class="date-div">
+                <label for="dueDate">Date: </label>
+                <input type="text" class="custom-select" id="dueDate" name="dueDate"></div>
+            <div class="priority-div">
+                <label for="taskPriority">Priority: </label>
+                <select class="custom-select" id="taskPriority" required="">
+                    <option value="Low" selected="">Low</option>
+                    <option value="Medium">Medium</option>
+                    <option value="High">High</option>
+                </select>
+            </div>
+            <div class="project-div">
+                <label for="taskProject">Profile: </label>
+                <select class="custom-select" id="taskProject" required="">
+                    <option>Inbox</option>
+                </select>
+            </div>
         </div>
-        <div class="close-btn">Close></div>
-        <div class="submit-btn">Submit Entry</div>
+    </div>
+    <div class="form-buttons">
+        <div class="close btn">Close</div>
+        <div class="submit btn">Submit Entry</div>
     </div>`
 
     container.prepend(createEntryDiv);
@@ -51,13 +68,15 @@ addNewBtn.addEventListener('click', () => {
 // Updates the database and UI with the form data
 const updateEntryList = () => {
     // Get form values after "Submit" is clicked and create a new "CreateEntry" object from them
-    const submitBtn = document.querySelector('.submit-btn');
+    const submitBtn = document.querySelector('.submit');
     submitBtn.addEventListener('click', () => {
         const title = document.querySelector('#title').value;
         const desc = document.querySelector('#desc').value;
         const dueDate = document.querySelector('#dueDate').value;
+        const priority = document.querySelector('#taskPriority').value;
+        const profile = document.querySelector('#taskProject').value;
 
-        const newEntry = CreateEntry(title, desc, dueDate);
+        const newEntry = CreateEntry(title, desc, dueDate, priority);
 
         // Update Database
         database.push(newEntry);
@@ -97,8 +116,23 @@ const addNewEntryForm = () => {
         </div>
     </div>
     `
+    updatePriority(database[database.length-1].priority);
 };
 
 const removeForm = () => {
     document.querySelector('.entry').remove();
 };
+
+const updatePriority = (arg) => {
+    switch(arg) {
+        case 'Low':
+            document.querySelector('.todo-element[data-id="' + (database.length-1) + '"]').style.borderLeft = "5px solid green";
+            break;
+        case 'Medium':
+            document.querySelector('.todo-element[data-id="' + (database.length-1) + '"]').style.borderLeft = "5px solid orange";
+            break;
+        case 'High':
+            document.querySelector('.todo-element[data-id="' + (database.length-1) + '"]').style.borderLeft = "5px solid red";
+            break;
+      }
+}
