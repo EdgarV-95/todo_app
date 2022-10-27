6c76cbdaf25334404484a0ab5c43713cbdda6839
EdgarV-95 <edgar.veress@yahoo.com>
2022-10-27 09:55:26 +0100

    css pt4

--- script.js
+++ script.js
@@ -12,33 +12,27 @@ const addNewBtn = document.querySelector('.add-entry');
 
 // Form to add new entries to the database and UI
 addNewBtn.addEventListener('click', () => {
+    // Reduces opacity of the background while form is present
+    lowerOpacity();
 
-
-    document.querySelector('.side-bar').style.opacity = '0.2';
-    document.querySelector('.add-entry').style.opacity = '0.2';
-    document.querySelector('.container-title').style.opacity = '0.2';
-    document.querySelector('.todo-list').style.opacity = '0.2';
-
-
+    // Creates form
     const createEntryDiv = document.createElement('div');
     createEntryDiv.classList.add('entry');
     createEntryDiv.setAttribute('id', 'entry');
-    createEntryDiv.innerHTML =
-    `<div class="title-div">
-    <p>Title: </p>
-    <input type="text" id="title" name="title">
-    </div>
-    <div class="desc-div"><p>Description: </p><input type="text" id="desc" name="desc"></div>
+    createEntryDiv.innerHTML = `
+    <textarea class="title-div" id="title" name="title" placeholder="Title: "></textarea>
+    <textarea class="desc-div" id="desc" name="desc" placeholder="Description: "></textarea>
     <div class="date-div"><p>Date: </p><input type="text" id="dueDate" name="dueDate"></div>
     <div class="submit-btn">Submit Entry</div>
     `
-    // container.appendChild(createEntryDiv);
-    document.querySelector('.test').before(createEntryDiv);
+    container.appendChild(createEntryDiv);
+
     updateEntryList();
 });
 
 // Updates the database and UI with the form data
 const updateEntryList = () => {
+    // Get form values after "Submit" is clicked and create a new "CreateEntry" object from them
     const submitBtn = document.querySelector('.submit-btn');
     submitBtn.addEventListener('click', () => {
         const title = document.querySelector('#title').value;
@@ -51,32 +45,47 @@ const updateEntryList = () => {
         database.push(newEntry);
 
         // Update Display
-        addNewEntry = (() => {
-            const newEntry = document.createElement('div');
-            newEntry.classList.add('todo-element');
-            newEntry.setAttribute('data-id', database.length-1);
-            document.querySelector('.todo-list').appendChild(newEntry);
-            document.body.querySelector('.todo-element[data-id="' + (database.length-1) + '"]').innerHTML =
-            `
-            ${database[database.length-1].title}
-            ${database[database.length-1].description}
-            ${database[database.length-1].dueDate}
-            `
-        })();
-        
-        // Hide form after submit is clicked
-        removeForm = (() => {
-            document.querySelector('.entry').remove();
-        })();
+        addNewEntryForm()
 
-        document.querySelector('.side-bar').style.opacity = '1';
-        document.querySelector('.add-entry').style.opacity = '1';
-        document.querySelector('.container-title').style.opacity = '1';
-        document.querySelector('.todo-list').style.opacity = '1';
+        // Hide form after submit is clicked
+        removeForm()
+
+        // Returns opacity of the background back to normal after form is closed
+        increaseOpacity()
     });
 };
 
-document.querySelector('.test').addEventListener('click', () => {
-    console.log(database)
-    console.log(document.querySelectorAll('.todo-element'))
-})
+const lowerOpacity = () => {
+    document.querySelector('.side-bar').style.opacity = '0.2';
+    document.querySelector('.add-entry').style.opacity = '0.2';
+    document.querySelector('.container-title').style.opacity = '0.2';
+    document.querySelector('.todo-list').style.opacity = '0.2';
+}
+
+const increaseOpacity = () => {
+    document.querySelector('.side-bar').style.opacity = '1';
+    document.querySelector('.add-entry').style.opacity = '1';
+    document.querySelector('.container-title').style.opacity = '1';
+    document.querySelector('.todo-list').style.opacity = '1';
+}
+
+const addNewEntryForm = () => {
+    const newEntry = document.createElement('div');
+    newEntry.classList.add('todo-element');
+    newEntry.setAttribute('data-id', database.length-1);
+    document.querySelector('.todo-list').appendChild(newEntry);
+    document.body.querySelector('.todo-element[data-id="' + (database.length-1) + '"]').innerHTML =
+    `<div class="todo-title">${database[database.length-1].title}</div>
+    <div class="todo-desc">${database[database.length-1].description}</div>
+    <div class="todo-dueDate">${database[database.length-1].dueDate}</div>
+    `
+};
+
+const removeForm = () => {
+    document.querySelector('.entry').remove();
+};
+
+// document.querySelector('.test').addEventListener('click', () => {
+//     console.log(database)
+//     console.log(document.querySelectorAll('.todo-element'))
+// })
