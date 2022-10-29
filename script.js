46a8bd95b8fbb501df3a808b2da70e5c5c1633b3
EdgarV-95 <edgar.veress@yahoo.com>
2022-10-29 20:45:03 +0100

    trying different approach

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 const database = [];
 
 // Factory function for creating entries and their properties
-const CreateEntry = (title, description, dueDate, priority, profile) => {
-    return { title, description, dueDate, priority, profile };
+const CreateEntry = (id, title, description, dueDate, priority, profile) => {
+    return {id, title, description, dueDate, priority, profile };
 };
 
 // Query selectors
@@ -71,13 +71,14 @@ const updateEntryList = () => {
     // Get form values after "Submit" is clicked and create a new "CreateEntry" object from them
     const submitBtn = document.querySelector('.submit');
     submitBtn.addEventListener('click', () => {
+        const id = Date.now().toString(36) + Math.floor(Math.pow(10, 12) + Math.random() * 9*Math.pow(10, 12)).toString(36);
         const title = document.querySelector('#title').value;
         const desc = document.querySelector('#desc').value;
         const dueDate = document.querySelector('#dueDate').value;
         const priority = document.querySelector('#taskPriority').value;
         const profile = document.querySelector('#taskProject').value;
 
-        const newEntry = CreateEntry(title, desc, dueDate, priority);
+        const newEntry = CreateEntry(id, title, desc, dueDate, priority);
 
         // Update Database
         database.push(newEntry);
@@ -106,19 +107,18 @@ const increaseOpacity = () => {
 const addNewEntryForm = () => {
     const newEntry = document.createElement('div');
     newEntry.classList.add('todo-element');
-    newEntry.setAttribute('data-id', database.length-1);
+    newEntry.setAttribute('data-id', database[database.length-1].id);
     document.querySelector('.todo-list').appendChild(newEntry);
-    document.body.querySelector('.todo-element[data-id="' + (database.length-1) + '"]').innerHTML = `
+    document.body.querySelector('.todo-element[data-id="' + database[database.length-1].id + '"]').innerHTML = `
     <div class="todo-title">${database[database.length-1].title}</div>
     <div class="todo-icons">
         <div class="todo-date">${database[database.length-1].dueDate}</div>
-        <div class="todo-edit"><span class="material-icons edit">edit</span></div>
-        <div class="todo-priority"><span class="material-icons flag${database.length-1}">flag</span></div>
-        <div class="todo-delete"><span class="material-icons delete">delete</span></div>
+        <div class="todo-edit material-icons">edit</div>
+        <div class="todo-priority"><span class="material-icons flag${database[database.length-1].id}">flag</span></div>
+        <div class="todo-delete material-icons">delete</div>
     </div>
     `
-    updatePriority(database[database.length-1].priority)
-    deleteEntry();
+    updatePriority(database[database.length-1].priority);
 };
 
 const removeForm = () => {
@@ -128,16 +128,16 @@ const removeForm = () => {
 const updatePriority = (arg) => {
     switch(arg) {
         case 'Low':
-            document.querySelector('.todo-element[data-id="' + (database.length-1) + '"]').style.borderLeft = "5px solid green";
-            document.querySelector('.flag' + (database.length-1)).style.color = "green";
+            document.querySelector('.todo-element[data-id="' + (database[database.length-1].id) + '"]').style.borderLeft = "5px solid green";
+            document.querySelector('.flag' + (database[database.length-1].id)).style.color = "green";
             break;
         case 'Medium':
-            document.querySelector('.todo-element[data-id="' + (database.length-1) + '"]').style.borderLeft = "5px solid orange";
-            document.querySelector('.flag' + (database.length-1)).style.color = "orange";
+            document.querySelector('.todo-element[data-id="' + (database[database.length-1].id) + '"]').style.borderLeft = "5px solid orange";
+            document.querySelector('.flag' + (database[database.length-1].id)).style.color = "orange";
             break;
         case 'High':
-            document.querySelector('.todo-element[data-id="' + (database.length-1) + '"]').style.borderLeft = "5px solid red";
-            document.querySelector('.flag' + (database.length-1)).style.color = "red";
+            document.querySelector('.todo-element[data-id="' + (database[database.length-1].id) + '"]').style.borderLeft = "5px solid red";
+            document.querySelector('.flag' + (database[database.length-1].id)).style.color = "red";
             break;
       }
 }
@@ -157,15 +157,52 @@ const closeModal = () => {
 // WHY IS IT REMOVING TWO ELEMENTS FROM THE DB?
 // Check addNewEntryForm() from line 106
 const deleteEntry = () => {
-    document.querySelectorAll('.delete').forEach(el => {
-        el.addEventListener('click', (e) => {
-            const clickedDatasetID = e.target.parentElement.parentElement.parentElement.dataset.id
-            if (e.target.parentElement.parentElement.parentElement.dataset.id === clickedDatasetID) {
-                // Removes element from DB
-                database.splice(clickedDatasetID, 1)
-                // Removes element from UI
-                e.target.parentElement.parentElement.parentElement.remove()
-            }
-        })
+    
+    // const nodeList = document.querySelectorAll('.delete');
+    // console.log(nodeList)
+    // Array.from(nodeList).forEach(el => {
+    //     el.addEventListener('click', (e) => {
+    //         // console.log(e.parentElement.parentElement.parentElement.dataset.id)
+    //         console.log(e.target.parentElement.parentElement.parentElement.dataset.id)
+    //     })
+    // })
+
+
+
+    // const nodeList = document.querySelectorAll('.delete');
+    // nodeList.forEach(el => {
+    //     console.log(el);
+    //     // el.addEventListener('click', (e) => {
+    //     //     for(let i = 0; i < database.length; i++) {
+    //     //         // console.log(database[i].id)
+    //     //         // console.log(nodeList[i].parentElement.parentElement.parentElement.dataset.id)
+    //     //         if(database[i].id === e.target.parentElement.parentElement.parentElement.dataset.id)
+    //     //         // Removes element from DB
+    //     //         database.splice(database[i], 1);
+    //     //         // Removes element from UI
+    //     //         e.target.parentElement.parentElement.parentElement.remove();
+    //     //     }
+    //     // })
+    // })
+
+
+    // document.querySelectorAll('.delete').addEventListener('click', (e) => {
+    //     const clickedDatasetID = e.target.parentElement.parentElement.parentElement.dataset.id;
+    //     if (clickedDatasetID === dbID) {
+    //         // Removes element from DB
+    //         database.splice(clickedDatasetID, 1);
+    //         // Removes element from UI
+    //         e.target.parentElement.parentElement.parentElement.remove();
+    //     };
+    // });
+};
+
+
+const nodeList = document.querySelectorAll('.todo-delete');
+console.log(nodeList)
+Array.from(nodeList).forEach(el => {
+    el.addEventListener('click', (e) => {
+        // console.log(e.parentElement.parentElement.parentElement.dataset.id)
+        console.log(e.target.parentElement.parentElement.parentElement.dataset.id)
     })
-}
+})
