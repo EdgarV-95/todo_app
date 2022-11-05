c88a8e85286785b8475ff4d79b383c5e3aac7e15
EdgarV-95 <edgar.veress@yahoo.com>
2022-11-05 19:24:06 +0000

    chore; Updated with prettier

--- script.js
+++ script.js
@@ -18,51 +18,56 @@ let database = [];
 
 // Show form by removing .off class from .entry
 addForm.addEventListener('click', () => {
-    form.classList.remove('off');
-    UI.lowerOpacity();
+  form.classList.remove('off');
+  UI.lowerOpacity();
 });
 
 // Handle the form when submitted
 submit.addEventListener('click', () => {
+  // First submit the values entered
+  let id = Math.random() * 100000;
 
-    // First submit the values entered
-    let id = Math.random() * 100000;
+  const todo = new Todo(
+    id,
+    title.value,
+    description.value,
+    dueDate.value,
+    priority.value,
+    profile.value
+  );
+  database = [...database, todo];
 
-    const todo = new Todo(id, title.value, description.value, dueDate.value, priority.value, profile.value);
-    database = [...database, todo];
-
-    UI.displayTask();
-    UI.closeForm();
-    UI.showTaskDescription();
-    UI.deleteElementFromUI();
+  UI.displayTask();
+  UI.closeForm();
+  UI.showTaskDescription();
+  UI.deleteElementFromUI();
 });
 
 close.addEventListener('click', () => {
-    UI.closeForm();
+  UI.closeForm();
 });
 xBtn.addEventListener('click', () => {
-    UI.closeForm();
+  UI.closeForm();
 });
 
 // Todo class
 class Todo {
-    constructor(id, title, description, dueDate, priority, profile) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-        this.profile = profile;
-    };
-};
+  constructor(id, title, description, dueDate, priority, profile) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.profile = profile;
+  }
+}
 
 // Class to update UI elements
 class UI {
-
-    // Update the UI with the newly added element
-    static displayTask() {
-        let displayTask = database.map(item => {
-            return `
+  // Update the UI with the newly added element
+  static displayTask() {
+    let displayTask = database.map((item) => {
+      return `
             <div class="todo-task-container" data-id=${item.id}>
                 <div class="todo-element" data-id=${item.id}>
                 <div class="todo-title">${item.title}</div>
@@ -73,48 +78,57 @@ class UI {
                 </div>
                 </div>
             </div>
-            `
-        });
-        // Remove , from the array when appearing in UI
-        list.innerHTML = displayTask.join('');
-        UI.clearForm();
-        UI.updatePriority();
-    };
+            `;
+    });
+    // Remove , from the array when appearing in UI
+    list.innerHTML = displayTask.join('');
+    UI.clearForm();
+    UI.updatePriority();
+  }
 
-    static updatePriority() {
-        database.map(item => {
-            switch (item.priority) {
-                case 'Low':
-                    document.querySelector(`.todo-element[data-id="${item.id}"]`).style.borderLeft = '1.5vw solid rgb(64,115,214)';
-                    break;
-                case 'Medium':
-                    document.querySelector(`.todo-element[data-id="${item.id}"]`).style.borderLeft = '1.5vw solid rgb(245,156,24)';
-                    break;
-                case 'High':
-                    document.querySelector(`.todo-element[data-id="${item.id}"]`).style.borderLeft = '1.5vw solid rgb(222,75,74)';
-                    break;
-            };
-        });
-    };
+  static updatePriority() {
+    database.map((item) => {
+      switch (item.priority) {
+        case 'Low':
+          document.querySelector(
+            `.todo-element[data-id="${item.id}"]`
+          ).style.borderLeft = '1.5vw solid rgb(64,115,214)';
+          break;
+        case 'Medium':
+          document.querySelector(
+            `.todo-element[data-id="${item.id}"]`
+          ).style.borderLeft = '1.5vw solid rgb(245,156,24)';
+          break;
+        case 'High':
+          document.querySelector(
+            `.todo-element[data-id="${item.id}"]`
+          ).style.borderLeft = '1.5vw solid rgb(222,75,74)';
+          break;
+      }
+    });
+  }
 
-    static showTaskDescription() {
-        const allTasksArr = Array.from(document.querySelectorAll('.todo-task-container'));
-        allTasksArr.map(task => {
-            task.addEventListener('click', (e) => {
-                let todoDetailsList = task.querySelectorAll('.todo-details');
-                if (todoDetailsList.length > 0) {
-                    let detailsArr = [...todoDetailsList];
-                    detailsArr.map(item => item.remove());
-                }
-                else {
-                    const details = document.createElement('div');
-                    details.classList.add('todo-details');
-                    task.append(details);
-    
-                    database = database.filter(item => item.id !== e.target.dataset.id);
-                    database.map(el => {
-                        if(el.id === +(e.target.dataset.id)) {
-                            details.innerHTML = `
+  static showTaskDescription() {
+    const allTasksArr = Array.from(
+      document.querySelectorAll('.todo-task-container')
+    );
+    allTasksArr.map((task) => {
+      task.addEventListener('click', (e) => {
+        let todoDetailsList = task.querySelectorAll('.todo-details');
+        if (todoDetailsList.length > 0) {
+          let detailsArr = [...todoDetailsList];
+          detailsArr.map((item) => item.remove());
+        } else {
+          const details = document.createElement('div');
+          details.classList.add('todo-details');
+          task.append(details);
+
+          database = database.filter(
+            (item) => item.id !== e.target.dataset.id
+          );
+          database.map((el) => {
+            if (el.id === +e.target.dataset.id) {
+              details.innerHTML = `
                             <div class="desc-left">
                                 <div class="desc-desc">Description: </div>
                                 <p class="desc-nl">${el.description}</p>
@@ -124,56 +138,58 @@ class UI {
                                 <div class="desc-priority">Priority: ${el.priority}</div>
                             </div>
                             `;
-                        };
-                    });
-                };
-            });
-        });
-    };
+            }
+          });
+        }
+      });
+    });
+  }
 
-    // Remove elment from the UI if the event.target contains the .delete class
-    static deleteElementFromUI() {
-        list.addEventListener('click', (e) => {
-            if (e.target.classList.contains('delete')) {
-                e.target.parentNode.parentNode.parentNode.parentNode.remove();
-            };
-            let btnId = e.target.parentNode.parentNode.parentNode.parentNode.dataset.id;
-            UI.deleteElementFromDB(btnId);
-        })
-    };
+  // Remove elment from the UI if the event.target contains the .delete class
+  static deleteElementFromUI() {
+    list.addEventListener('click', (e) => {
+      if (e.target.classList.contains('delete')) {
+        e.target.parentNode.parentNode.parentNode.parentNode.remove();
+      }
+      let btnId =
+        e.target.parentNode.parentNode.parentNode.parentNode.dataset
+          .id;
+      UI.deleteElementFromDB(btnId);
+    });
+  }
 
-    // Removes the element from the database.
-    // Filters through the array and only returns the elements that do not have the id given as a parameter
-    static deleteElementFromDB(id) {
-        database = database.filter(item => item.id !== +id);
-    };
+  // Removes the element from the database.
+  // Filters through the array and only returns the elements that do not have the id given as a parameter
+  static deleteElementFromDB(id) {
+    database = database.filter((item) => item.id !== +id);
+  }
 
-    // Clear the form values
-    static clearForm() {
-        title.value = '';
-        description.value = '';
-        dueDate.value = getCurrentDate;
-        priority.value = 'Low';
-        profile.value = 'Inbox';
-    };
+  // Clear the form values
+  static clearForm() {
+    title.value = '';
+    description.value = '';
+    dueDate.value = getCurrentDate;
+    priority.value = 'Low';
+    profile.value = 'Inbox';
+  }
 
-    static closeForm() {
-        form.classList.add('off');
-        UI.increaseOpacity();
-    };
+  static closeForm() {
+    form.classList.add('off');
+    UI.increaseOpacity();
+  }
 
-    static lowerOpacity = () => {
-        document.querySelector('.side-bar').style.opacity = '0.2';
-        document.querySelector('.main-body').style.opacity = '0.2';
-    };
-    
-    static increaseOpacity = () => {
-        document.querySelector('.side-bar').style.opacity = '1';
-        document.querySelector('.main-body').style.opacity = '1';
-    };
-};
+  static lowerOpacity = () => {
+    document.querySelector('.side-bar').style.opacity = '0.2';
+    document.querySelector('.main-body').style.opacity = '0.2';
+  };
+
+  static increaseOpacity = () => {
+    document.querySelector('.side-bar').style.opacity = '1';
+    document.querySelector('.main-body').style.opacity = '1';
+  };
+}
 
 // Update date to today
-let getCurrentDate = (new Date()).toISOString().split('T')[0];
+let getCurrentDate = new Date().toISOString().split('T')[0];
 dueDate.value = getCurrentDate;
 dueDate.min = getCurrentDate;
